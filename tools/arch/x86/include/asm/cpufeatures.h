d5556c3ef3f6e9a39f0b8ae57ed7497713a53083
H. Peter Anvin (Intel) <hpa@zytor.com>
2021-04-29 21:43:01 -0700

    x86/cpufeature: add cpu feature bits for FRED and LKGS
    Add the CPU feature bits for FRED (Fast/Flexible Return and Event
    Delivery) and LKGS (Load "Kernel" GS).
    
    Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

--- tools/arch/x86/include/asm/cpufeatures.h
+++ tools/arch/x86/include/asm/cpufeatures.h
@@ -306,6 +306,8 @@
 /* Intel-defined CPU features, CPUID level 0x00000007:1 (EAX), word 12 */
 #define X86_FEATURE_AVX_VNNI		(12*32+ 4) /* AVX VNNI instructions */
 #define X86_FEATURE_AVX512_BF16		(12*32+ 5) /* AVX512 BFLOAT16 instructions */
+#define X86_FEATURE_FRED		(12*32+ 17) /* Fast Return/Exception Delivery */
+#define X86_FEATURE_LKGS		(12*32+ 18) /* Load "kernel" (userspace) gs */
 
 /* AMD-defined CPU features, CPUID level 0x80000008 (EBX), word 13 */
 #define X86_FEATURE_CLZERO		(13*32+ 0) /* CLZERO instruction */
