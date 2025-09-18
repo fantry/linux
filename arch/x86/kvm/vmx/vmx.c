xinli-intel/linux-fred-public

KVM: VMX: Remove read_msr() defined in <asm/kvm_host.h>
KVM should utilize the native MSR APIs instead of the generic MSR APIs,
which are also applicable to paravirtualized CPUs.

Consequently, read_msr() defined in <asm/kvm_host.h> is merely an alias
for native_rdmsrq(), so remove it.

Signed-off-by: Xin Li (Intel) <xin@zytor.com>

arch/x86/kvm/vmx/vmx.c
@@ -1335,8 +1335,8 @@ void vmx_prepare_switch_to_guest(struct kvm_vcpu *vcpu)
 	} else {
 		savesegment(fs, fs_sel);
 		savesegment(gs, gs_sel);
-		fs_base = read_msr(MSR_FS_BASE);
-		vmx->msr_host_kernel_gs_base = read_msr(MSR_KERNEL_GS_BASE);
+		fs_base = native_rdmsrq(MSR_FS_BASE);
+		vmx->msr_host_kernel_gs_base = native_rdmsrq(MSR_KERNEL_GS_BASE);
 	}
 
 	wrmsrq(MSR_KERNEL_GS_BASE, vmx->msr_guest_kernel_gs_base);

fantry/linux

arch/x86/kvm/vmx/vmx.c
@@ -1288,8 +1288,8 @@ void vmx_prepare_switch_to_guest(struct kvm_vcpu *vcpu)
 	} else {
 		savesegment(fs, fs_sel);
 		savesegment(gs, gs_sel);
-		fs_base = read_msr(MSR_FS_BASE);
-		vt->msr_host_kernel_gs_base = read_msr(MSR_KERNEL_GS_BASE);
+		fs_base = native_rdmsrq(MSR_FS_BASE);
+		vt->msr_host_kernel_gs_base = native_rdmsrq(MSR_KERNEL_GS_BASE);
 	}
 
 	wrmsrq(MSR_KERNEL_GS_BASE, vmx->msr_guest_kernel_gs_base);